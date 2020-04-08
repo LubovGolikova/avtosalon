3ac148adee78c5334eb4a18bdcbd278de6557a8d
eromasters <evromastersx@gmail.com>
2020-04-08 19:12:53 +0300

    nav-bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,24 @@
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  <link rel="stylesheet" href="{{asset('styles/style.css')}}">
 </head>
-<body>
+<body class="home-page">
 <header id="headerId">
   <div class="container-inner-top d-md-flex flex-row justify-content-center align-items-center">
+
       <a href="" class="d-block">г.Запорожье</a>
       <a href="" class="d-block">+38 067 929 32 22</a>
       <a href="" class="d-block">+38 095 319 34 08</a>
-  </div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+     <div class="social">
+        <a href="#"><i class="fa fa fa-facebook-square" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+     </div>
+  </div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light-blue">
     <div class="container">
+     @csrf
      <div class="navbar-brand"> <a href="/">
          <img src="{{asset('assets/images/logo-2.png')}}" alt=""/>
          </a>
@@ -27,10 +35,10 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav mx-auto align-md-items-center">
           <a class="nav-item nav-link active" href="#">Главная <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">О нас</a>
-          <a class="nav-item nav-link" href="#">Цены</a>
-          <a class="nav-item nav-link" href="#">Отзывы</a>
-          <a class="nav-item nav-link" href="#">Услуги</a>
+          <a class="nav-item nav-link" href="#">О нас<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Цены<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Отзывы<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Услуги<span class="sr-only">(current)</span></a>
         </div>
       </div>
       </div>
