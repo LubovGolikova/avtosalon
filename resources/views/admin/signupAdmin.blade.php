066d35fe7eb78e7aca73bb7f3df4018748913071
eromasters <evromastersx@gmail.com>
2020-04-25 17:01:58 +0300

    add nav

--- resources/views/admin/signupAdmin.blade.php
+++ resources/views/admin/signupAdmin.blade.php
@@ -13,29 +13,28 @@
                   </tr>
               </thead>
               <tbody>
-        @foreach($signups as $signup)
-        <tr>
-            <div class="container-signup row">
-            <td>{{$loop->iteration}}</td>
-            <td><p>{{$signup->name}} </p></td>
-            <td><p>{{$signup->phone}}</p></td>
-            <td><p>{{\Illuminate\Support\Carbon::parse($signup->created_at)->format('d.m.Y')}}</p></td>
-            </div>
-            <td>
-            <form action="/signup/admin/{{$signup->id}}" method="POST">
-                @csrf
-               @method('DELETE')
-            <button  class="btn btn-secondary">Удалить</button>
-            </form>
-            </td>
-        </tr>
-        @endforeach
-          </tbody>
-          </table>
+                    @foreach($signups as $signup)
+                    <tr>
+                        <div class="container-signup row">
+                        <td>{{$loop->iteration}}</td>
+                        <td><p>{{$signup->name}} </p></td>
+                        <td><p>{{$signup->phone}}</p></td>
+                        <td><p>{{\Illuminate\Support\Carbon::parse($signup->created_at)->format('d.m.Y')}}</p></td>
+                        </div>
+                        <td>
+                            <form action="/signup/admin/{{$signup->id}}" method="POST">
+                                @csrf
+                               @method('DELETE')
+                            <button  class="btn btn-secondary">Удалить</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+              </tbody>
+       </table>
         <div class="container-pagination">
             {{$signups->links()}}
         </div>
-
     </div>
 
 </div>
